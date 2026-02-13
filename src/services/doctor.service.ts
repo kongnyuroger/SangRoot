bd42a667248187a071e0dccd99f9442e167cb027
Kongnyu Roger <kongnyuroger711@gmail.com>
2026-02-13 14:32:07 +0100

    consumed endpoint for doctors to register donors

---
+++ src/services/doctor.service.ts
@@ -0,0 +1,21 @@
+import { api } from "../lib/api";
+
+type RegisterDonorDto = {
+  name: string;
+  email?: string;
+  phone: string;
+  bloodGroup: string;
+  address: string;
+  city: string;
+  state: string;
+  pincode: string;
+  latitude: number;
+  longitude: number;
+  isAvailable?: boolean;
+};
+
+export async function registerDonor(data: RegisterDonorDto) {
+  return api.post("doctors/donors", { json: data }).json();
+}
+
+export default { registerDonor };
